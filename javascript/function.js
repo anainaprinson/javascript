96095dc8e99a890c77cb807e1c84ecf27fdc25bc
anainaprinson <150226848+anainaprinson@users.noreply.github.com>
2024-05-21 14:46:56 +0530

    Add files via upload

---
+++ javascript/function.js
@@ -0,0 +1,13 @@
+function add(a,b){
+    let c=a+b;
+    console.log(c);
+}
+
+add(1,2);
+
+function add(fname,lname){
+    let fullname=fname+lname;
+    console.log(fullname);
+
+}
+add("anaina","bijo")
---
+++ javascript/script.js
@@ -0,0 +1,90 @@
+//console.log("hello world!!");
+//let a=1;
+//let b=2;
+//console.log(a+b);
+//{
+
+ //   var a=10;
+
+//}
+// let a=10;
+// let b=20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
+// console.log(a%b);
+//postfix increment
+// let a=2;
+// let b=a++;
+// console.log(a);
+// console.log(b);
+//prefix increment
+// let c=2;
+// let d=++c;
+// console.log(c);
+// console.log(d);
+//object
+// let car={
+//     MODEL:"BENS",
+//     color:"black"
+
+// };
+// console.log(car);
+// console.log(car.model);
+// let persons={
+//     fname:"anaina prinson",
+//     address:{
+//         street:"yxf",
+//         place:"abc",
+//         pin:"123",
+// },
+//  age:10
+// };
+// console.log(person.address.place);
+// console.log(person.age)
+//  var toothbursh=new Object();
+//  toothbursh.brand="close up"
+//  toothbursh.color="yellow";
+//  console.log('toothbrush.brand')
+
+
+// //concatination
+//  let fname="devu";
+//  let lname="kunji";
+//  console.log(`${fname} ${lname}`)
+//  let m=5;
+//  let n=6;
+ 
+//  console.log(`The sum = ${m+n}`)
+ //array
+ let fruits=["apple","mango","orange",23]
+//  console.log(fruits[0]);
+// console.log(fruits.length)
+fruits[2]="orange";
+fruits.unshift("pear");
+console.log(fruits);
+fruits.pop();
+console.log(fruits)
+// array of object
+let bsc=[
+    {
+    fname:"anaina",
+    age: 19,
+},
+{
+    fname:"devika",
+    age:"11",
+
+
+},
+{
+    frame:"aleena",
+    age:40,
+},
+];
+console.log(bsc[0].age);
+
+
+
+
